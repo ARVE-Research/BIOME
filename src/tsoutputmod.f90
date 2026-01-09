8d0f6a45662e917cec2b8c2eddd27599893e09bd
Jed O. Kaplan <jed.kaplan@arve-research.com>
2026-01-09 10:52:38 -0700

    added text file output routine

---
+++ src/tsoutputmod.f90
@@ -0,0 +1,26 @@
+module tsoutputmod
+
+implicit none
+
+public :: writedailymetvars
+
+contains
+
+! ----------------------------------------------------------------------------------------------------------------
+
+subroutine writedailymetvars(m,d,dmet)
+
+use parametersmod, only : dmetfile_unit
+use typesmod, only : metvars_daily
+
+integer, intent(in)             :: m      ! month number
+integer, intent(in)             :: d      ! day number
+type(metvars_daily), intent(in) :: dmet
+
+write(dmetfile_unit,*)m,d,dmet%tday,dmet%tnight,dmet%prec,dmet%snow,dmet%melt,dmet%swe,dmet%fsnow,dmet%asnow,dmet%Bsw
+
+end subroutine writedailymetvars
+
+! ----------------------------------------------------------------------------------------------------------------
+
+end module tsoutputmod
